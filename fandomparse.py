4779e515e083b928a1637b456beb482e73ad9633
abramov_ms <abramov.ms@phystech.edu>
2021-04-20 22:53:31 +0300

    Implement fandom website parser

---
+++ fandomparse.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class FandomParserException(Exception):
+    pass
+
+
+class FandomParser:
+    _fandomUrl = "https://losyash-library.fandom.com/ru/wiki/"
+
+    def __init__(self, episode_name):
+        if episode_name[-2] == "-":
+            episode_name = episode_name[:-2]
+        self._webpage_url = FandomParser._fandomUrl + \
+            episode_name.replace(" ", "_")
+
+        request = requests.get(self._webpage_url)
+        self._soup = BeautifulSoup(request.text, "lxml")
+
+    def get_section_list(self, section_name):
+        section_id = section_name.replace(" ", "_")
+        section_span = self._soup.find("span", {"id": section_id})
+        if section_span is None:
+            raise FandomParserException("section '{}' not found".format(
+                section_name))
+
+        section_header = section_span.parent
+        section_ul_list = section_header.next_sibling.next_sibling
+        section_list = [element.text.strip() for element in
+                        section_ul_list.find_all("li")
+                        if element.text.strip() != ""]
+
+        return section_list
